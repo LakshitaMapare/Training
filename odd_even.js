866839db4d9373e9821ea0ff37d4ab7ff0ad0c4b
Lakshitamapare <Lakshitamapare@gmail.com>
2026-07-14 22:41:40 +0530

    New file

---
+++ odd_even.js
@@ -0,0 +1,11 @@
+let Num=[22,35,23,54,67];
+for(let i=0;i<Num.length;i++)
+    {
+    if(Num[i]%2==0) {
+    console.log(Num[i],"is even No");
+}
+    
+    else {
+        console.log(Num[i],"is odd No");
+    }
+}
