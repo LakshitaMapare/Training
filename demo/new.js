a14a9490de9690a48baf62a933b11c7d3f9d5610
Lakshitamapare <Lakshitamapare@gmail.com>
2026-07-15 10:01:38 +0530

    feat/latest code

--- demo/new.js
+++ demo/new.js
@@ -1,2 +1,16 @@
-console.log("hello world program")
-console.log("new repository")
+let num1 = 20;
+let num2 = 10;
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+
+console.log("First Number:", num1);
+console.log("Second Number:", num2);
+
+console.log("Addition =", addition);
+console.log("Subtraction =", subtraction);
+console.log("Multiplication =", multiplication);
+console.log("Division =", division);
+ 
